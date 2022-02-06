e7a63e06a2c6561eb202b925ada9c7fd08c9f6a0
dazzlemon <30831873+dazzlemon@users.noreply.github.com>
2022-02-06 22:57:35 +0200

    Getting ready for supercharged dart

--- lr1/bin/lr1.dart
+++ lr1/bin/lr1.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 import 'dart:math';
 import 'package:collection/collection.dart';
+// import supercharged dart
 
 List<List<T>> makeMatrix<T>(int rows, int cols, T fill) =>
-	Iterable<List<T>>.generate(
+	List<List<T>>.filled(
 		rows,
-		(_) => List<T>.filled(cols, fill)
-	).toList();
+		List<T>.filled(cols, fill)
+	);
 
 String _toString(element) => element.toString();
 
@@ -18,30 +19,30 @@ void printMatrix<T>(List<List<T>> matrix, {
   String Function(T) elementToString=_toString
 }) {
   var stringElements = matrix.map((row) =>
-    row.map((e) => elementToString(e)).toList()
-  ).toList();
-  var maxWidth = stringElements.flattened.map((s) => s.length).reduce(max);
+    row.map((e) => elementToString(e))
+  );
+  var maxWidth = stringElements.flattened.map((s) => s.length).max();
 
   String alignedElement(String s) =>
     ' ' * (maxWidth - s.length) + s;
 
   void printRow(List<String> row) {
     stdout.write(rowPrefix);
-    row.take(row.length - 1).forEach((element) =>
+    row.withoutLast.forEach((element) =>
       stdout.write(alignedElement(element) + elementDelimiter)
     );
     stdout.write(alignedElement(row.last) + rowSuffix + rowDelimiter);
   }
 
-  stringElements.take(matrix.length - 1).forEach(printRow);
+  stringElements.withoutLast.forEach(printRow);
   printRow(stringElements.last);
 }
 
 void main(List<String> arguments) {
   var matrix = makeMatrix(3, 3, 0);
   var r = Random();
-  for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 3; j++) {
+  for (var i in 0.until(3)) {
+    for (var j in 0.until(3)) {
       matrix[i][j] = r.nextInt(10000000) - 5000000;
     } 
   } 
