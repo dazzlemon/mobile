388770d5c47ccd038a87708e0335901f459f9020
dazzlemon <daanill13470@gmail.com>
2022-02-08 20:36:26 +0200

    more functional stuff

--- lr1/bin/lr1.dart
+++ lr1/bin/lr1.dart
@@ -1,50 +1,57 @@
 import 'dart:io';
 import 'dart:math';
 import 'package:collection/collection.dart';
-// import supercharged dart
+import 'package:list_ext/list_ext.dart';
 
 List<List<T>> makeMatrix<T>(int rows, int cols, T fill) =>
-	List<List<T>>.filled(
+	List<List<T>>.generate(
 		rows,
-		List<T>.filled(cols, fill)
+		(i) => List<T>.filled(cols, fill)
 	);
 
 String _toString(element) => element.toString();
 
 void printMatrix<T>(List<List<T>> matrix, {
   String elementDelimiter=' ',
+  // elementAlignment = Left
+  //                                    | Right
+  //                                    | Center
+  //                                    | None
   String rowDelimiter='\n',
   String rowPrefix='',
   String rowSuffix='',
   String Function(T) elementToString=_toString
 }) {
-  var stringElements = matrix.map((row) =>
-    row.map((e) => elementToString(e))
-  );
-  var maxWidth = stringElements.flattened.map((s) => s.length).max();
+  var stringElements = matrix
+    .map((row) => row
+      .map((e) => elementToString(e)));
+  var maxWidth = stringElements
+    .flattened
+    .map((s) => s.length)
+    .max();
 
   String alignedElement(String s) =>
-    ' ' * (maxWidth - s.length) + s;
+    s.padLeft(maxWidth, ' ');
 
-  void printRow(List<String> row) {
-    stdout.write(rowPrefix);
-    row.withoutLast.forEach((element) =>
-      stdout.write(alignedElement(element) + elementDelimiter)
-    );
-    stdout.write(alignedElement(row.last) + rowSuffix + rowDelimiter);
-  }
+  String rowToString(Iterable<String> row)
+    => rowPrefix
+    +   row.map(alignedElement)
+              .join(elementDelimiter)
+    +   rowSuffix;
 
-  stringElements.withoutLast.forEach(printRow);
-  printRow(stringElements.last);
+  stdout.write(stringElements
+    .map(rowToString)
+    .join(rowDelimiter));
 }
 
 void main(List<String> arguments) {
   var matrix = makeMatrix(3, 3, 0);
   var r = Random();
-  for (var i in 0.until(3)) {
-    for (var j in 0.until(3)) {
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
       matrix[i][j] = r.nextInt(10000000) - 5000000;
-    } 
-  } 
+    }
+  }
   printMatrix(matrix, elementDelimiter: ' | ');
+  print('');
 }
