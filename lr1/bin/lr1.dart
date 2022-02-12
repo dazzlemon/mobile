ca72c8e935d8a553a8604f381578276197ad1d89
dazzlemon <daanill13470@gmail.com>
2022-02-12 19:37:14 +0200

    cli args

--- lr1/bin/lr1.dart
+++ lr1/bin/lr1.dart
@@ -1,9 +1,27 @@
 import 'package:lr1/matrix.dart';
 import 'package:lr1/magic_square.dart';
 
-void main(List<String> arguments) {
-	var size = 3;
-	magicSquares(size)
-		.map(matrixToString)
-		.forEach((m) => print(m + '\n'));
+void printUsage() {
+	print('this app will print all magic squares of provided size');
+	print('it can accept arguments >3, but it will take very long time to produce results');
+	print('argument=2 wont produce any results because none exist');
+}
+
+void main(List<String> args) {
+	if (args.length != 1) {
+		print('wrong amount of arguments');
+		printUsage();
+	} else {
+		var size = int.tryParse(args.first);
+		if (size == 2) {
+			print('no magic squares of size 2 exist');
+		} else if (size != null) {
+			magicSquares(size)
+				.map(matrixToString)
+				.forEach((m) => print(m + '\n'));
+		} else {
+			print('bad argument');
+			printUsage();
+		}
+	}
 }
