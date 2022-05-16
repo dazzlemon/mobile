e4e9d7c03d3d3f6371beb67b36e54a334fc2557e
dazzlemon <daanill13470@gmail.com>
2022-05-16 22:35:35 +0300

    almost fixed

--- lr3/lib/main.dart
+++ lr3/lib/main.dart
@@ -1,69 +1,41 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'dart:developer';
 
-///////// Data classes
-class Note {
-  String title;
-  String level;
+class Todo {
   DateTime dateCreate = DateTime.now();
   DateTime dateModify = DateTime.now();
-  Note({this.title = '', this.level = 'none'});
+	String details;
+	String title;
+  String level;
+  bool done;
+
+  Todo({
+		this.done = false, this.details = '', this.title = '', this.level = 'none'
+	});
+
   update(other) {
-    this
-      ..title = other.title
-      ..level = other.level
-      ..dateModify = DateTime.now();
-  }
-}
-
-class Memo extends Note {
-  String details = "";
-  Memo({this.details = '', title = '', level = 'none'})
-      : super(title: title, level: level);
-  @override
-  update(other) {
-    super.update(other);
-    this.details = other.details;
-  }
-
-  @override
-  toString() => "memo||$title||$level||$details";
-}
-
-mixin Check {
-  bool done = false;
-}
-
-class Todo extends Memo with Check {
-  Todo({isDone = false, details = '', title = '', level = 'none'})
-      : super(title: title, level: level) {
-    done = isDone;
-  }
-  @override
-  update(other) {
-    super.update(other);
-    this.done = other.done;
+		this..title   = other.title
+        ..level   = other.level
+    	  ..done    = other.done
+    	  ..details = other.details
+        ..dateModify = DateTime.now();
   }
 
   @override
   toString() => "todo||$done||$title||$level||$details";
 }
 
-//TODO class Event extend Todo with Date
 //Container
 class Notes {
-  List<Note> items = [];
+  List<Todo> items = [
+		// test items
+		Todo(done: false, details: 'string1 \n string2', title:'todo1',
+			level: 'high'),
+		Todo(done: true, details: 'string1 \n string2', title:'todo2'),
+	];
   var _curLevel = '*'; //filter value, * - all
-  Notes() {
-//test items
-// items.add(Memo(details:'string1 \n string2', title:'memo 1'));
-// items.add(Memo(details:'string1 \n string2', title:'memo 2'));
-// items.add(Memo(details:'string1 \n string2', title:'memo3',level:'high'));
-// items.add(Todo(isDone:false, details:'string1 \n string2', title:'todo1',level:'high'));
-// items.add(Todo(isDone:true, details:'string1 \n string2',title:'todo2'));
-// items.add(Memo(details:'string1 \n string2', title:'memo4',level:'high'));
-  }
   get count => items.where(_filter).length;
   get item => (int i) => items.where(_filter).toList()[i];
   get levels => items.map((x) => x.level).toSet().toList();
@@ -72,45 +44,38 @@ class Notes {
     _curLevel = lev;
   }
 
-  bool _filter(x) {
-    if (_curLevel == '*')
-      return true;
-    else
-      return _curLevel == x.level;
-  }
+  bool _filter(x) => _curLevel == '*' || _curLevel == x.level;
 
   load() async {
-//TODO load from file
+		//TODO load from file
     final directory = await getApplicationDocumentsDirectory();
-    print("load from $directory");
+    log("load from $directory");
     final file = File(directory.path + "/notes.txt");
     items.clear();
     for (String x in file.readAsLinesSync()) {
-      print(x);
+      log(x);
       var s = x.split("||");
       switch (s[0]) {
         case "todo":
           items.add(Todo(
-              isDone: s[1] == "true", title: s[2], level: s[3], details: s[4]));
+            done: s[1] == "true", title: s[2], level: s[3], details: s[4]));
           break;
-        case "memo":
-          items.add(Memo(title: s[1], level: s[2], details: s[3]));
       }
     }
-    print("load end");
+    log("load end");
   }
 
   save() async {
     try {
       final directory = await getApplicationDocumentsDirectory();
-      print("save to $directory");
+      log("save to $directory");
       final file = File(directory.path + "/notes.txt");
       file.openWrite();
       for (var x in items) {
         file.writeAsStringSync(x.toString() + '\n', mode: FileMode.append);
       }
     } catch (e) {
-      print("Error while saving the file: " + e.toString());
+      log("Error while saving the file: " + e.toString());
     }
   }
 
@@ -118,9 +83,8 @@ class Notes {
     items.remove(item);
   }
 
-  add<T extends Note>() {
-    final factories = <Type, Function>{Memo: () => Memo(), Todo: () => Todo()};
-    final one = (factories[T]!)();
+  add() {
+    final one = Todo();
     items.add(one);
     return one;
   }
@@ -128,24 +92,28 @@ class Notes {
 
 ////////////////////// Widgets
 class ItemPage extends StatefulWidget {
-  final _item, _levels;
-  const ItemPage(this._item, this._levels);
+  final Todo _item;
+	final List<String> _levels;
+  const ItemPage(this._item, this._levels, {Key? key}) : super(key: key);
+
   @override
   _ItemPageState createState() => _ItemPageState(_item, _levels);
 }
 
 class _ItemPageState extends State<ItemPage> {
-  final Memo _item;
-  var _levels;
-  bool _check = false;
+  final Todo _item;
+  final List<String> _levels;
   final cntTitle = TextEditingController();
   final cntLevel = TextEditingController();
   final cntDetails = TextEditingController();
+	
+  bool _check = false;
+
   _ItemPageState(this._item, this._levels) {
     cntTitle.text = _item.title;
     cntLevel.text = _item.level;
     cntDetails.text = _item.details;
-    if (_item is Todo) _check = (_item as Todo).done;
+    _check = _item.done;
   }
   @override
   void dispose() {
@@ -162,11 +130,11 @@ class _ItemPageState extends State<ItemPage> {
   showDelDialog(context) {
 // set up the AlertDialog
     AlertDialog alert = AlertDialog(
-      title: Text("Confirm"),
-      content: Text("Would you like to remove item?"),
+      title: const Text("Confirm"),
+      content: const Text("Would you like to remove item?"),
       actions: [
         TextButton(
-          child: Text("Cancel"),
+          child: const Text("Cancel"),
           onPressed: () {
             Navigator.pop(
               context,
@@ -174,7 +142,7 @@ class _ItemPageState extends State<ItemPage> {
           },
         ),
         TextButton(
-          child: Text("Yes"),
+          child: const Text("Yes"),
           onPressed: () {
             Navigator.pop(
               context,
@@ -199,32 +167,31 @@ class _ItemPageState extends State<ItemPage> {
       appBar: AppBar(
         title: Text('${_item.runtimeType}'),
         actions: [
-          new IconButton(
-              icon: new Icon(Icons.done_outline_rounded),
+          IconButton(
+              icon: const Icon(Icons.done_outline_rounded),
               onPressed: () {
                 Navigator.pop(context, _upd());
               }),
-          new IconButton(
-              icon: new Icon(Icons.cancel),
+          IconButton(
+              icon: const Icon(Icons.cancel),
               onPressed: () => showDelDialog(context))
         ],
       ),
       body: Center(
         child: Column(
           children: [
-            if (_item is Todo)
-              CheckboxListTile(
-                value: _check,
-                title: Text("Done"),
-                onChanged: (chk) {
-                  setState(() {
-                    _check = chk!;
-                  });
-                },
-              ),
+						CheckboxListTile(
+							value: _check,
+							title: const Text("Done"),
+							onChanged: (chk) {
+								setState(() {
+									_check = chk!;
+								});
+							},
+						),
             TextField(
               controller: cntTitle,
-              decoration: InputDecoration(labelText: 'Title'),
+              decoration: const InputDecoration(labelText: 'Title'),
             ),
             ListTile(
               trailing: PopupMenuButton(
@@ -243,13 +210,13 @@ class _ItemPageState extends State<ItemPage> {
                       ]),
               title: TextField(
                 controller: cntLevel,
-                decoration: InputDecoration(labelText: 'Level'),
+                decoration: const InputDecoration(labelText: 'Level'),
               ),
             ),
             Expanded(
                 child: TextField(
               controller: cntDetails,
-              decoration: InputDecoration(labelText: 'Details'),
+              decoration: const InputDecoration(labelText: 'Details'),
               minLines: 5,
               maxLines: 10,
             )),
@@ -259,20 +226,16 @@ class _ItemPageState extends State<ItemPage> {
     );
   }
 
-  _upd() {
-    if (_item is Todo)
-      return Todo(
-          isDone: _check,
-          details: cntDetails.text,
-          title: cntTitle.text,
-          level: cntLevel.text);
-    if (_item is Memo)
-      return Memo(
-          details: cntDetails.text, title: cntTitle.text, level: cntLevel.text);
-  }
+  _upd() => Todo( done: _check
+		            , details: cntDetails.text
+							  , title: cntTitle.text
+							  , level: cntLevel.text
+							  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
 // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -281,13 +244,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(title: 'Simple notes'),
+      home: const HomePage(title: 'Simple notes'),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key, this.title = ""}) : super(key: key);
+  const HomePage({Key? key, this.title = ""}) : super(key: key);
   final String title;
   @override
   _HomePageState createState() => _HomePageState();
@@ -301,18 +264,20 @@ class _HomePageState extends State<HomePage> {
         MaterialPageRoute(
           builder: (BuildContext context) => ItemPage(item, notes.levels),
         ));
-    if (result != null)
+    if (result != null) {
       setState(() {
-        if (result == "del")
+        if (result == "del") {
           notes.remove(item);
-        else
+				} else {
           item.update(result);
+				}
       });
+		}
   }
 
   listItem(i, context) {
     final one = notes.item(i);
-    if (one is Todo)
+    if (one is Todo) {
       return ListTile(
         leading: Icon(
           Icons.done,
@@ -324,14 +289,14 @@ class _HomePageState extends State<HomePage> {
         dense: true,
         onTap: () => showItem(one),
       );
-    if (one is Memo)
-      return ListTile(
-        title: Text(one.title),
-        subtitle: Text(one.level),
-        trailing: Text(one.dateCreate.toString().substring(0, 10)),
-        dense: true,
-        onTap: () => showItem(one),
-      );
+		}
+		return ListTile(
+			title: Text(one.title),
+			subtitle: Text(one.level),
+			trailing: Text(one.dateCreate.toString().substring(0, 10)),
+			dense: true,
+			onTap: () => showItem(one),
+		);
   }
 
   _HomePageState();
@@ -356,17 +321,17 @@ class _HomePageState extends State<HomePage> {
             child: ListView(
 //itemExtent: 40.0,
           children: [
-            DrawerHeader(
+            const DrawerHeader(
               child: Text('Main menu'),
               decoration: BoxDecoration(
                 color: Colors.blue,
               ),
             ),
-            ListTile(title: Text('LEVELS')),
+            const ListTile(title: Text('LEVELS')),
             for (var x in ["*", ...notes.levels])
               ListTile(
                 title: Text(x),
-                contentPadding: EdgeInsets.only(left: 50),
+                contentPadding: const EdgeInsets.only(left: 50),
                 dense: true,
                 selected: notes.filter == x,
                 onTap: () => setState(() {
@@ -374,48 +339,39 @@ class _HomePageState extends State<HomePage> {
                   notes.setFilter(x);
                 }),
               ),
-            Divider(),
+            const Divider(),
             ListTile(
-                title: Text('Save'),
-                leading: Icon(Icons.save_outlined),
+                title: const Text('Save'),
+                leading: const Icon(Icons.save_outlined),
                 onTap: () {
                   notes.save();
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: const Text('Saved.'),
+                    const SnackBar(
+                      content: Text('Saved.'),
                     ),
                   );
-                  ;
                 }),
             ListTile(
-                title: Text('Load'),
-                leading: Icon(Icons.file_upload),
+                title: const Text('Load'),
+                leading: const Icon(Icons.file_upload),
                 onTap: () async {
                   await notes.load();
                   setState(() {});
                 }),
-            Divider(),
-            ListTile(title: Text('About...')),
+            const Divider(),
+            const ListTile(title: Text('About...')),
           ],
         )),
         floatingActionButton:
             Column(mainAxisAlignment: MainAxisAlignment.end, children: [
           Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
             FloatingActionButton(
-              onPressed: () => showItem(notes.add<Memo>()),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15.0))),
-              heroTag: 'memo',
-              tooltip: 'new memo item',
-              child: Text('+memo'),
-            ),
-            FloatingActionButton(
-              onPressed: () => showItem(notes.add<Todo>()),
-              shape: RoundedRectangleBorder(
+              onPressed: () => showItem(notes.add()),
+              shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(15.0))),
               heroTag: 'todo',
               tooltip: 'new todo item',
-              child: Text('+todo'),
+              child: const Text('+todo'),
             )
           ])
         ]));
@@ -424,5 +380,5 @@ class _HomePageState extends State<HomePage> {
 
 ///////////////////////
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
